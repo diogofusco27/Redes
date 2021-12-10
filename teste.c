4e3d4e8df5f8001519556c36a1ebfa9489f4e6b0
Bruno <32464654+brunogmm@users.noreply.github.com>
2021-12-10 12:58:15 +0000

    Update teste.c

--- teste.c
+++ teste.c
@@ -6,25 +6,24 @@
 #include <stdlib.h>
 
 int main (){
-printf("existe1");
 
 FILE *fp;
 int check;
-char* dirname = "GROUP/user00009";
+char* dirname = "GROUP/user00005";
 char buff[15];
 
-printf("existe2");
+
 
 check = mkdir(dirname,0777);
 
-printf("existe3");
-fp=fopen("GROUP/user00009/user_ID.txt", "rw");
 
-/*if( access( "USERS/user00005/user_ID.txt", F_OK ) == 0 ) {
+fp=fopen("GROUP/user00005/user_ID.txt", "w+");
+
+if( access( "GROUP/user00005/user_ID.txt", F_OK ) == 0 ) {
     printf("existe");
 } else {
     printf("nao existe");
-}*/
+}
 fprintf(fp, "839");
 
 fgets(buff, 15, fp);
@@ -33,6 +32,5 @@ fgets(buff, 15, fp);
 printf("%s\n",buff);
 
 //fclose(fp);
-
 return 0;
 }
